d6fe6356d11643aa329ac1d8a2405e76d1d6026d
aguperezpala <aguperezpala@b48e2724-961f-11dd-95f0-dd7fc3bc1b4e>
2008-10-16 00:23:35 +0000

    empezando struct user

---
+++ trunk/src/users/user.c
@@ -0,0 +1,5 @@
+#include "user.c"
+
+
+struct _user_t {
+	char DNI[MAX_DNI_SIZE];
---
+++ trunk/src/users/user.h
@@ -0,0 +1,18 @@
+#ifndef USER_C_H
+#define USER_C_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Practicamente estos defines no los vamos a tocar nunca*/
+#define MAX_DNI_SIZE	9
+
+
+typedef struct _user_t user_t;
+
+
+
+
+
+
+#endif
