91fab05f86c4b1201d8e845b2cacc33a5f14a165
aguperezpala <aguperezpala@b48e2724-961f-11dd-95f0-dd7fc3bc1b4e>
2008-11-03 15:07:25 +0000

    haciendo el parser

--- trunk/src/qparser/parser.h
+++ trunk/src/qparser/parser.h
@@ -5,12 +5,54 @@
 #define PARSER_H
 
 #include <QString>
+#include "../debug.h"
+
+
+
+/*el final de sms siempre va a ser 2 endl*/
+#define PARSER_END_SMS	"\n\n"
+
+/*!Modificar esto*/
+#define PARSER_BEGIN_SMS	"[SMSBackup"
+
+/*aca vamos a definir los "campos" que queremos obtener desde el archivo*/
+/*!	cada campo va a ser FIELD = xxxxx	*/
+
+
+#define PARSER_NUMBER_FIELD	"Number"	/*hasta encontrar un \n*/
+#define PARSER_TEXT_FIELD	";"			/*hasta encontrar un \n y despues un char!=';'*/
+
 
 class Parser
 {
-	public:
-		Parser();
-		
+public:
+	Parser();
+	
+	/*esta funcion va a tomar el primer sms del QString source y ELIMINARLO
+	de source una vez sacado el sms.
+	
+		RETURNS:
+				NULL == ERROR
+				first sms != NULL
+	
+	Nota: GENERA MEMORIA... tener en cuenta
+	*/
+	QString* getFirstSms (QString& source);
+	
+	/*!Estas funciones nos van a devolver determinados campos del sms.
+		REQUIRES:
+				QString != NULL
+		RETURNS:
+				NULL == ERROR
+				field != NULL
+	NOTA: GENERA MEMORIA... debe ser liberada luego
+	*/
+	
+	/*obtenemos el numero limpio (sin comillas ni espacios)*/
+	QString* getNumberField (QString *sms);
+	
+	/*obtenemos el texto*/
+	QString* getTextField (QString *sms);
 	
 	
 	private:
