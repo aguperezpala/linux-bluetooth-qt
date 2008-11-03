4179bd8696d540269dd0b2bc530d983c6e38490d
aguperezpala <aguperezpala@b48e2724-961f-11dd-95f0-dd7fc3bc1b4e>
2008-11-03 15:07:25 +0000

    haciendo el parser

--- src/qparser/parser.cpp
+++ src/qparser/parser.cpp
@@ -4,3 +4,96 @@ Parser::Parser ()
 {
 
 }
+
+QString* Parser::getFirstSms (QString& source)
+{
+	QString *result = NULL;
+	int begin = -1, end = -1;
+	
+		
+	if (!source.isNull() && !source.isEmpty()) {
+		/*ahora buscamos el principio del sms*/
+		begin = source.indexOf (QString (PARSER_BEGIN_SMS), 0, Qt::CaseInsensitive);
+		
+		if (begin > 0)	/*tomamos el final del "sms"*/
+			end = source.indexOf (QString (PARSER_END_SMS), begin, Qt::CaseInsensitive);
+		
+		if (end > begin) {
+			/*ahora debemos generar el result*/
+			result = new QString (source.mid (begin, end - begin));
+			/*y borramos del source el sms*/
+			source.remove (begin, end - begin);
+		}
+	}
+	
+	return result;
+}
+
+
+/*obtenemos el numero limpio (sin comillas ni espacios)*/
+QString* Parser::getNumberField (QString *sms)
+{
+	QString *result = NULL;
+	int begin = -1, end = -1;
+	QString aux(PARSER_NUMBER_FIELD);
+	/*sabemos que sms != NULL*/
+	ASSERT (sms != NULL);
+	
+	if (sms != NULL && !sms->isEmpty()) {
+		aux.append (" =");
+		begin = sms->indexOf (aux, 0, Qt::CaseInsensitive);
+		if (begin > 0)
+			end = sms->indexOf (QString ("\n"), begin, Qt::CaseInsensitive);
+		
+		if (end > begin) {
+			result = new QString ("");
+			if (result != NULL) {
+				/*agregamos el numero*/
+				result->append (sms->mid (begin, end - begin));
+				/*reemplazamos las comillas*/
+				result->replace (QChar ('"'), QString (""));
+				/*sacamos los espacios*/
+				result->replace (QString(" "), QString (""));
+			}
+		}
+	}
+	return result;
+}
+
+/*obtenemos el texto*/
+QString* Parser::getTextField (QString *sms)
+{
+	QString *result = NULL;
+	int begin = -1, end = -1;
+	QString aux(PARSER_TEXT_FIELD);
+	QChar c = '0';
+	/*sabemos que sms != NULL*/
+	ASSERT (sms != NULL);
+	
+	if (sms != NULL && !sms->isEmpty()) {
+		begin = sms->indexOf (aux, 0, Qt::CaseInsensitive);
+		
+		if (begin > 0) {
+			/*ahora debemos chequear que el proximo char sea distinto de ';'*/
+			end = sms->indexOf (QString ("\n"), begin, Qt::CaseInsensitive);
+			c = (*sms)[end+1];
+			while (c == ';') {
+				end = sms->indexOf (QString ("\n"), end+1, Qt::CaseInsensitive);
+				c = (*sms)[end+1];
+			}	
+			/*en teoria ya tenemos el tamaño del texto, desde begin hasta end*/
+			if (end > begin) {
+				result = new QString ("");
+				if (result != NULL) {
+					/*agregamos el numero*/
+					result->append (sms->mid (begin, end - begin));
+					/*reemplazamos los ';'*/
+					result->replace (QString(";"), QString (""));
+				}
+			}
+		}
+	}
+	return result;
+}
+
+
