fd3e21ee7ce34851524ad50d1af3000ddd4eccd7
aguperezpala <aguperezpala@b48e2724-961f-11dd-95f0-dd7fc3bc1b4e>
2009-09-15 04:23:26 +0000

    agregando udatabase con los tests casi completados. Siguiendo trabajando

--- src/tester.h
+++ src/tester.h
@@ -4,7 +4,7 @@
 #include <assert.h>
 /* funciones tipicas */
 #define fail_if(x)		assert(!(x)); \
-printf("fail_if ("" x " ") pass \n");
+printf("fail_if (" "x"  ") pass \n");
 #define fail_unless(x)		assert(x)
 
 
