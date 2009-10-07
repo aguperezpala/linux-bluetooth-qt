db807a7dce6ae35bdbcf392eb14bdabba92ac4c8
aguperezpala <aguperezpala@b48e2724-961f-11dd-95f0-dd7fc3bc1b4e>
2009-10-07 18:07:24 +0000

    aregando mas cambios (obexreceiver / obexparser )

---
+++ src/obexreceiver/obexparser/obexparser.h
@@ -0,0 +1,58 @@
+/*! Modulo que parsea segun el pseudo protocolo las cosas desde un buffer
+ * determinado
+*/
+#ifndef CUSER_H
+#define CUSER_H
+
+
+#include <QString>
+#include "../consts.h"
+#include "../debug.h"
+
+
+/* no es el tamaño verdadero, es el size "visual" */
+#define MAC_ADDR_SIZE 			MAC_SIZE
+class CUser{
+	public:
+		/* Constructor que pide de una los datos, en caso de que sean
+		 * NULL los string no los copia.
+		 * REQUIRES:
+		 *	mac != NULL && mac->size == MAC_ADDR_SIZE / * cacaso * /
+		 * NOTE: copia memoria, liberar con lo que se llamo 
+		 */
+		CUser (QString * nick, QString * mac);
+		
+		/* GETS
+		 * NOTE: devuelve el puntero NO COPIA MEMORIA => NO liberarla
+		 */
+		const QString& getNick (void) const {return nick;};
+		const QString& getMAC (void) const {return MAC;};
+		
+		/* Funcion que copia un usuario desde un puntero devolviendo
+		 * una nueva instancia.
+		 * NOTE: GENERA memoria
+		 */
+		CUser * copy(void);
+		
+		/*! VAMOS A DETERMINAR QUE 2 USUARIOS SON IGUALES POR EL NUMERO
+		  * DE MAC */
+		/* REQUIRES: CUser other != NULL */
+		bool operator==(const CUser & other);
+		/* REQUIRES: CUser other != NULL */
+		bool operator!=(const CUser & other);
+		
+		/*!DEBUG*/
+		#ifdef __DEBUG
+		void printUser();
+		#endif
+		
+		~CUser();
+		
+	private:
+		/*aca vamos a definir los campos de la clase usuario*/
+		QString nick;
+		QString MAC;
+		
+};
+
+#endif
---
+++ src/obexreceiver/obexparser/obexparser.pro
@@ -0,0 +1,3 @@
+DEFINES += __DEBUG
+HEADERS += filec.h cuser.h ../debug.h 
+SOURCES += filec.c cuser.cpp test.cpp        
