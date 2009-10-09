f2ef080fb1d99ca52c816cc7a87fe78ba1a1284d
aguperezpala <aguperezpala@b48e2724-961f-11dd-95f0-dd7fc3bc1b4e>
2009-10-09 00:30:46 +0000

    algunos cambios mas... empezando con el mainwidget

--- trunk/src/dispobjmanager/mainwidget.h
+++ trunk/src/dispobjmanager/mainwidget.h
@@ -1,18 +1,25 @@
-#ifndef LOADWIDGET_H
-#define LOADWIDGET_H
+/*! Esta es la ventana principal, donde vamos a tener la tabla de objetos
+  * recibidos, y a su vez las opciones para controlar las diferentes ventanas
+  * de control.
+  */
+
+#ifndef MAINWIDGET_H
+#define MAINWIDGET_H
 
 #include <QWidget>
 #include <QPushButton>
 #include <QtGui>
 #include <QtUiTools>
 
-QT_BEGIN_NAMESPACE
-class QLabel;
-class QSpinBox;
-QT_END_NAMESPACE
 
+#include "../consts.h"
+#include "../debug.h"
+#include "picturewindow/picturewindow.h"
+#include "picturewindow/picwincontrol.h"
+#include "textwindow/textwindow.h"
+#include "textwindow/txtwincontrol.h"
 
-class MainWidget : public QWidget
+class MainWidget : public QWidget, private Ui::MainWidget
 {
     Q_OBJECT
 
@@ -23,7 +30,10 @@ public slots:
 	void on_button1_clicked ();
 
 private:
-	QPushButton *button1;
+	PictureWindow * pw;		/* ventana de fotos */
+	PicWinControl * pwc;		/* ventana de control de fotos */
+	TextWindow * tx;		/* ventana de texto */
+	TxtWinControl * twc;		/* ventana de control de texto */
 };
 
 #endif
