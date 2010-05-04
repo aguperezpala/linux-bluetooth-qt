bd96549b7b80b1e86751fd8fd0d91a189fee839d
aguperezpala <aguperezpala@b48e2724-961f-11dd-95f0-dd7fc3bc1b4e>
2010-05-04 19:33:29 +0000

    version vieja, con obexpush (ultima actualizacion

--- openpushd/obexpushd-0.9-source/src/vfromat-converter/vnote-parser.h
+++ openpushd/obexpushd-0.9-source/src/vfromat-converter/vnote-parser.h
@@ -1,16 +1,21 @@
 #ifndef VNOTE_PARSER_H
 #define VNOTE_PARSER_H
 
+#define _GNU_SOURCE
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <glib.h>
 #include <ctype.h>
+#include <iconv.h>
 
 
 
 #define VNP_TABLE_FILE		"vnote_table.txt"
 #define VNP_RARE_CHARS		"C2 C3"
+#define VNP_WITHE_SPACES	" \t\r\f"
+#define VNP_ACCEPTED_CHARS	"ABCDEF0123456789=abcdef"
 #define VNOTE_SPECIAL_CHAR	'='
 
 typedef struct _vnParser vnParser_t;
@@ -31,7 +36,7 @@ void vnp_destroy(vnParser_t * vnp);
  *	decoded != NULL		if no error
  *	NULL			on error
  */
-char * vnp_parsearse (vnParser_t * vnp, char * data);
+char * vnp_parse (vnParser_t * vnp, char * data);
 
 
 
