1e85ba32598b799b9b22276cdef7afa5100e8b0f
aguperezpala <aguperezpala@b48e2724-961f-11dd-95f0-dd7fc3bc1b4e>
2010-05-06 15:20:45 +0000

    agregando diagrama y funcion del celu

---
+++ celu/hello/ServerManager.java
@@ -0,0 +1,197 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hello;
+
+import java.util.Vector;
+import javax.bluetooth.RemoteDevice;
+import javax.microedition.lcdui.Form;
+
+/**
+ *
+ * @author agustin
+ */
+public class ServerManager implements Runnable {
+    
+    private boolean isRunning = false;
+    private Vector serverList = new Vector();
+    private Thread smThread = null;
+    private DeviceFinder devFinder = null;
+    private Form statusForm = null;
+
+
+
+    /** <<                   Funciones privadas                  >> */
+
+    /** Funcion que intenta conectarse a un RemoteDevice, e intenta obtener
+     * la lista de macs de los servidores.
+     * @return  True if success | False if not
+     * @param   rD != null
+     */
+
+    private boolean getServersList(RemoteDevice rD) {
+        boolean result = false;
+        BtConnection btCon = null;
+        String url = null;
+        String serverResp = null;
+        int status = 0;
+        Vector auxVec = null;
+
+        if (rD == null)
+            return result;
+
+        /* generamos la url */
+        //btspp://6A54017D1A00:11;authenticate=false;encrypt=false;master=false
+        url = new String("");
+        url += CityBluetooth.PROTOCOL_ACCEPTED + "://" + rD.getBluetoothAddress();
+        url += ":" + Integer.toString(CityBluetooth.ACCEPTED_PORT) + ";";
+        url += CityBluetooth.PARAMS;
+
+        /* intentamos conectarnos */
+        btCon = new BtConnection(url, this.statusForm,
+                CityBluetooth.CONN_MAX_TIME_OUT);
+        if (btCon == null || btCon.getStatus() < 0) {
+            this.statusForm.append("SM: getServersList: btCon creation err\n");
+            return result;
+        }
+
+        /* estamos conectados... vamos a tratar de mandarle la peticion de
+         * servidores */
+        status = btCon.sendData("requ:15:get_server_list");
+        if (status < 0) {
+            this.statusForm.append("SM: getServersList: btCon sending err\n");
+            return result;
+        }
+
+        /* si llegamos aca => pudimos mandar correctamente, ahora intentamos
+         * ver y desifrar que nos mando el server
+         */
+        serverResp = btCon.reciveData();
+        
+        if (serverResp == null || BtParser.parseRecvData(serverResp) < 0) {
+            this.statusForm.append("SM: getServersList: btCon receiving err\n");
+            return result;
+        }
+
+        /* obtenemos el mensaje y lo parseamos entonces */
+        auxVec = BtParser.parseServerList(serverResp);
+        if (auxVec == null) {
+            this.statusForm.append("SM: getServersList: parsing ServerList err\n");
+            return result;
+        }
+        
+        /* si llegamos aca esta todo joya => agregamos todos los servers que
+         * no tengamos ya */
+        for (int i = 0; i < auxVec.size(); i++)
+            if (!this.serverList.contains(auxVec.elementAt(i)))
+                    /* lo agregamos */
+                    this.serverList.addElement(auxVec.elementAt(i));
+        
+
+        return result;
+    }
+
+
+
+
+ /** <<                   Funciones publicas                  >> */
+
+
+
+
+    public ServerManager(Form stF) {
+        this.statusForm = stF;
+        this.devFinder = new DeviceFinder(this.statusForm,
+                CityBluetooth.RFCOMM_UUID, CityBluetooth.ATTRIBUTES,
+                CityBluetooth.ACCEPTED_PORT);
+
+        /* comenzamos a buscar de antemano */
+        if (this.devFinder != null)
+            this.devFinder.startFindDevices();
+    }
+
+    public void startSearchingServers() {
+        /* verificamos que no este buscando */
+        if(this.smThread != null)
+            return;
+        /* generamos todo */
+        this.smThread = new Thread(this);
+        this.smThread.start();
+    }
+
+    public void stopSearching() {
+        if (this.smThread != null && (this.smThread.isAlive() ||
+                this.isRunning)) {
+            this.smThread.interrupt();
+            this.smThread = null;
+        }
+        this.isRunning = false;
+    }
+
+    public Vector getServerVector() {
+        return this.serverList;
+    }
+
+
+    public void run() {
+        boolean haveFound = false;
+        Vector workDevs = null;
+        RemoteDevice rD = null;
+
+
+        this.isRunning = true;
+
+        /* vamos a esperar primero que todo a que el devFinder termine de
+         * buscar dispositivos... una vez que termine vamos a intentar
+         * conectarnos a cada uno de ellos para obtener la lista de servers
+         */
+        while(!haveFound) {
+            /* buscamos y esperamos esperamos */
+            if (!this.devFinder.isSearching())
+                this.devFinder.startFindDevices();
+            while (this.devFinder.isSearching())
+                try {
+                    Thread.sleep(500);
+                } catch (Exception e){}
+
+            /* ya dejo de buscar, verificamos si tenemos dispositivos */
+            workDevs = this.devFinder.getWorkingDevices();
+            if (workDevs == null || workDevs.size() == 0) {
+                /* no encontramos nada => hacemos una pausa y volvemos a buscar */
+                try {
+                    /* deberiamos setear una constante cada cuanto volver a
+                     * buscar */
+                    Thread.sleep(10000);
+                } catch (Exception e){}
+                this.statusForm.append("ServerManager: no encontramos devs\n");
+                continue;
+            }
+            /* si estamos aca es porque SI encontramos dispositivos =>
+             * vamos a intentar conectarnos e intentar obtener la lista
+             * de verdaderos servidores */
+            for (int i = 0; i < workDevs.size(); i++) {
+                rD = null;
+                rD = (RemoteDevice) workDevs.elementAt(i);
+
+                /* ahora vamos a verificar si podemos obtener la lista o no
+                 * de este server... */
+                if (this.getServersList(rD) == false) {
+                    /* no pudimos => lo eliminamos del vector y seguimos */
+                    workDevs.removeElementAt(i);
+                    i--;
+                    continue;
+                }
+                /* si estamos aca es porque ya obtuvimos lo que queriamos
+                 * y tenemos en this.serverList la lista con todos los
+                 * servidores => salimos diretcamente */
+                haveFound = true;
+                break;
+            }
+        }
+        this.isRunning = false;
+    }
+
+
+}
