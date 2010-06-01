6498904d90b67ccc4da6b83eb88a0aae305f3dfc
aguperezpala <aguperezpala@b48e2724-961f-11dd-95f0-dd7fc3bc1b4e>
2010-06-01 00:58:12 +0000

    cambiando el nombre a codegenerator por codeadmin

--- trunk/src/btreceiver/btcodeadmin/btcodeadmin.cpp
+++ trunk/src/btreceiver/btcodeadmin/btcodeadmin.cpp
@@ -1,5 +1,6 @@
 #include "btcodegenerator.h"
 
+
 /* Converts a hexadecimal string to integer
 * REQUIRES:
 * 	xs != NULL
@@ -11,7 +12,7 @@
 * 	4    - Conversion is in process but abnormally terminated by 
 * 		illegal hexadecimal character
 */
-static int xtoi(const char* xs, unsigned int* result)
+int BTCodeAdmin::xtoi(const char* xs, unsigned int* result)
 {
 	size_t szlen = 0;
 	int i, xv, fact;
@@ -26,8 +27,8 @@ static int xtoi(const char* xs, unsigned int* result)
 		// Converting more than 32bit hexadecimal value?
 		if (szlen>BTCG_CODE_SIZE) return 2; // exit
 			
-		// Begin conversion here
-		*result = 0;
+			// Begin conversion here
+			*result = 0;
 		fact = 1;
 		
 		// Run until no more character to convert
@@ -66,20 +67,17 @@ static int xtoi(const char* xs, unsigned int* result)
 }
 
 
-/*! vamos a definir los codigos de la siguiente forma:
-* XXXYYYYY
-* Donde XXX va a ser el numero del codigo (numCode) generado y YYYYY va a ser 
-* el numero tal que es igual a: numDay * numMonth * numCode
-* Representado en hexadecimal, y va a tener que ser enviado como tal.
-*
-*/
+
+
+
+
 
 /* Funcion que determina si un codigo es valido o no 
 * RETURNS:
 * 	true	if code its valid
 * 	false	otherwise
 */
-bool btcg_is_valid_code(string &data)
+bool BTCodeAdmin::isCodeValid(string &code)
 {
 	time_t t = time(NULL);
 	struct tm *lt = localtime(&t);
@@ -111,13 +109,36 @@ bool btcg_is_valid_code(string &data)
 	return false;
 }
 
-/* Funcion que genera un codigo determinado teniendo en cuenta un tamaño pre
-* seteado (BTCG_CODE_SIZE).
+/* Funcion que determina si un codifo fue ya usado o no
+* RETURNS:
+* 	true	if was used
+* 	false 	otherwise
+*/
+bool BTCodeAdmin::wasUsed(string &code)
+{
+	return (this->hash.find(code) != map::end);
+}
+
+/* Funcion que agrega un codigo ya usado a la lista de codigos
+* usados */
+void BTCodeAdmin::addUsedCode(string &code)
+{
+	this->hash.insert( pair<string,bool>(code,true) );
+}
+
+/* Funcion que setea un codigo como no usado... */
+void BTCodeAdmin::setCodeUnused(string &code)
+{
+	this->hash.erase(code);
+}
+
+/* Funcion que genera un N codigos teniendo en cuenta un tamaño 
+* pre seteado (BTCG_CODE_SIZE).
 * RETURNS:
 * 	< 0	on error
 * 	0	if success
 */
-int btcg_gen_code(vector<string> &codes, int N)
+int BTCodeAdmin::genCodes(vector<string> &codes, int N)
 {
 	unsigned int codeNum = 7, cod = 0;
 	char result[BTCG_CODE_SIZE] = {0};
@@ -142,7 +163,7 @@ int btcg_gen_code(vector<string> &codes, int N)
 		
 		/* ahora seteamos el valor del codigo */
 		cod = ((unsigned int) lt->tm_mon) * ((unsigned int) lt->tm_mday) *
-			codeNum;
+		codeNum;
 		sprintf(result, "%X", cod);
 		for (i = 5 - strlen(result); i > 0; i--)
 			code.append("0");
@@ -153,4 +174,67 @@ int btcg_gen_code(vector<string> &codes, int N)
 	}
 	
 	return 0;
+	
 }
+
+/* Funcion que guarda la lista de codigos usados hasta
+* el momento en un archivo
+* NOTE: Rescribe el archivo si existe
+*/
+void BTCodeAdmin::toFile(string &fname)
+{
+	fstream filestr;
+	map<string,bool>::iterator it;
+	string aux = "";
+	
+	filestr.open(fname.c_str(), fstream::out);
+	
+	if (!filestr.good())
+		return;
+	
+	
+	for(it = this->hash.begin(); it != this->hash.end(); ++it) {
+		aux = (*it).first;
+		aux.append('\n');
+		filestr.write(aux.c_str(), aux.size());
+		if(filestr.fail())
+			break;
+	}
+	
+	filestr.close();
+}
+
+/* Funcion que carga una lista de codigos usados desde un
+* archivo determinado.
+*/
+void BTCodeAdmin::fromFile(string &fname)
+{
+	fstream filestr;
+	char line[BTCG_CODE_SIZE + 2];
+	int size = BTCG_CODE_SIZE + 2;
+	string aux = "";
+	
+	filestr.open(fname.c_str(), fstream::in);
+	
+	if (!filestr.good())
+		return;
+	while(!filestr.fail()){
+		filestr.getline(line, size);
+		aux = line;
+		if(!filestr.eof())
+			this->hash.insert( pair<string,bool>(aux, true) );
+		else
+			break;
+	}
+	
+	
+	filestr.close();
+}
+
+/* destructor */
+BTCodeAdmin::~BTCodeAdmin(void)
+{
+	
+}
+
+
--- trunk/src/btreceiver/btreceiver/btreceiver.pro
+++ trunk/src/btreceiver/btreceiver/btreceiver.pro
@@ -1,14 +1,14 @@
 INCLUDEPATH +=  ../.. ../../udatabase ../../cuser ../btprotocol ../btpaket . \
-../btcodegenerator/
+../btcodeadmin/
 
 DEFINES += __DEBUG
 
 HEADERS += ../../udatabase/udatabase.h ../../debug.h ../../tester.h ../btpaket/btpaket.h \
-../btprotocol/btprotocol.h ../btcodegenerator/btcodegenerator.h \
+../btprotocol/btprotocol.h ../btcodeadmin/btcodeadmin.h \
 ../../cuser/cuser.h ../../consts.h btreceiver.h
 
 SOURCES += ../../cuser/cuser.cpp ../../udatabase/udatabase.cpp \
-../btcodegenerator/btcodegenerator.cpp \
+../btcodeadmin/btcodeadmin.cpp \
 ../btpaket/btpaket.cpp ../btprotocol/btprotocol.cpp btreceiver.cpp test.cpp 
 
 LIBS += -lbluetooth -lbt-manager
